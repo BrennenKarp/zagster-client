58274c0a7e9a0332924522aeeb519b77ad923f2c
Brennen Karp <brennenk45@gmail.com>
2018-11-05 15:22:32 -0800

    Added new html

--- main.js
+++ main.js
@@ -1,5 +1,13 @@
+
+const BASE_URL = "https://zagster-service.herokuapp.com"
+
 $(updateView)
 
 function updateView() {
-    alert("Sea horses aren't real!")
+    $.getJSON(BASE_URL + "/rides/count" , updateRideCount)
+}
+
+function updateRideCount(data) {
+    numberOfRides = data.count
+    $("h2#rideCount").html(numberOfRides)
 }
